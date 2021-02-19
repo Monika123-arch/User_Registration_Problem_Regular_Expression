316c115b47e95b5c1b6639e7cda49e5cc939bd28
Monika <monikadkale98@gmail.com>
2021-02-19 15:24:02 +0530

    added new program

--- UserRegisterProblem.java
+++ UserRegisterProblem.java
@@ -5,24 +5,25 @@ import java.util.Scanner;
 public class UserRegisterProblem {
 
 	public static void main(String[] args) {
-		//Declare last name
+                //declare password
 		Scanner sc = new Scanner(System.in);
 		String name;
-		System.out.println("enter last name :");
+		System.out.println("enter passward :");
 		name = sc.nextLine();
-		lastname(name);
+		passward2(name);
 	}
-	public static void lastname(String name) {
-		//Declare pattern
-		Pattern p = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}+$");
-		Matcher m = p.matcher(name);
+	public static void passward2(String name) {
+                 //declare pattern
+                //Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
+		Pattern p = Pattern.compile("^([A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$");
+                Matcher m = p.matcher(name);
 		if(m.find())
 		{
-			System.out.println("lastname is valid");
+			System.out.println("passward is valid");
 		}
 		else
 		{
-			System.out.println("lastname is invalid");
+			System.out.println("passward is invalid");
 		}
 		System.out.println("length :"+name.length());
 	}
