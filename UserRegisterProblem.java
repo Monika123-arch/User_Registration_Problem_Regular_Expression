6f627c1ad3b141644fd4f47113a7a162743bbc9d
Monika <monikadkale98@gmail.com>
2021-02-24 12:01:34 +0530

    added new program

--- UserRegisterProblem.java
+++ UserRegisterProblem.java
@@ -1,28 +1,29 @@
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Scanner;
 
 public class UserRegisterProblem {
 
 	public static void main(String[] args) {
-		//Declare last name
+
 		Scanner sc = new Scanner(System.in);
 		String name;
-		System.out.println("enter last name :");
+		System.out.println("enter passward :");
 		name = sc.nextLine();
-		lastname(name);
+		passward3(name);
 	}
-	public static void lastname(String name) {
-		//Declare pattern
-		Pattern p = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}+$");
-		Matcher m = p.matcher(name);
+	public static void passward3(String name) {
+
+                //Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
+		Pattern p = Pattern.compile("^[\\@\\&\\$\\#\\_\\%\\,\\.](?=.*[A-Z])(?=.*[0-9]).{8,}$");
+                Matcher m = p.matcher(name);
 		if(m.find())
 		{
-			System.out.println("lastname is valid");
+			System.out.println("passward is valid");
 		}
 		else
 		{
-			System.out.println("lastname is invalid");
+			System.out.println("passward is invalid");
 		}
 		System.out.println("length :"+name.length());
 	}
